7d064dc63738726d045bc8fd556976c34a9649b3
o-vuong <oscarvuong@gmail.com>
2019-07-28 14:00:21 -0700

    react app added

---
+++ notes/src/components/NotesContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class NotesContainer extends Component {
+  render() {
+    return (
+      <div>
+        Notes
+      </div>
+    )
+  }
+}
+
+export default NotesContainer
